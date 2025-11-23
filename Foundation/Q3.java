aff3499c83af5b09678fb793ab7a10795073e378
greeshmitha2702 <bingumallagreeshmitha@gmail.com>
2025-11-23 11:21:21 +0530

    fibonacci up to n

---
+++ Foundation/Q3.java
@@ -0,0 +1,19 @@
+//To print Fibonacci series up to n terms
+package Foundation;
+import java.util.Scanner;
+public class Q3 {
+    public static void main(String[] args) {
+        Scanner s=new Scanner(System.in);
+        System.out.println("Enter the number of terms: ");
+        int n = s.nextInt();
+        int n1=0,n2=1;
+        System.out.println("Fibonacci Series up to "+n+" terms:");
+        for(int i=0;i<n;i++){
+            System.out.println(n1);
+            int n3=n1+n2;
+            n1=n2;
+            n2=n3;
+        }
+        s.close();
+    }
+}
