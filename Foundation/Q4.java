499af0f2d52e8d4899fb31d90c44b5ca555baeea
greeshmitha2702 <bingumallagreeshmitha@gmail.com>
2025-11-23 11:36:40 +0530

    no of digits

---
+++ Foundation/Q4.java
@@ -0,0 +1,19 @@
+//To count number of digits in a number
+package Foundation;
+
+import java.util.Scanner;
+
+public class Q4 {
+    public static void main(String[] args) {
+        Scanner s=new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int n=s.nextInt();
+        int count=0;
+        while(n!=0){
+            count++;
+            n/=10;
+        }
+        System.out.println("Number of digits: " + count);
+        s.close();
+    }
+}
