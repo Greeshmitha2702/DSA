cdb898e34b0878ec871754ef164d6a2b2c4672ff
greeshmitha2702 <bingumallagreeshmitha@gmail.com>
2025-11-25 23:21:20 +0530

    rotation of a number

--- Foundation/Q8.java
+++ Foundation/Q8.java
@@ -1,19 +1,34 @@
+//rotation of a number
+//positive rotation->clockwise
+//negative rotation->anticlockwise
 package DSA.Foundation;
 public class Q8 {
     public static void main(String[] args) {
         int number=12345;
+        int rotation=-2;
         int count=0;
         int temp=number;
         while(temp!=0){
             count++;
             temp/=10;
         }
-        int rotation=3;
-        int divisor=(int) Math.pow(10,rotation);
-        int remainder=number%divisor;
-        int rem_digits=number/divisor;
-        int result=remainder*(int)Math.pow(10, count-rotation);
-        int final_ans=result+rem_digits;
-        System.out.println(final_ans);
+        rotation=rotation%count;//for rotations greater than number of digits
+        if(rotation<0){//for negative rotations
+            rotation+=count;
+        }
+        int div=1;
+        int mult=1;
+        for(int i=1;i<=count;i++){
+            if(i<=rotation){
+                div*=10;
+            }
+            else{
+                mult*=10;
+            }
+        }
+        int quotient=number/div;
+        int remainder=number%div;
+        int result=(remainder*mult)+quotient;
+        System.out.println(result);
     }
 }
