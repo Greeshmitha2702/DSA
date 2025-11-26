7150b628db8cf1f3e25beb70fbe1f5530db54c3e
greeshmitha2702 <bingumallagreeshmitha@gmail.com>
2025-11-26 16:36:04 +0530

    change in 1st pattern

--- Patterns/Q1.java
+++ Patterns/Q1.java
@@ -1,19 +1,20 @@
+/*
+*
+* *
+* * *
+* * * *
+* * * * *
+*/
 package Patterns;
 public class Q1 {
     public static void main(String[] args) {
         int n = 5; // Number of rows
-
-        for (int i = 1; i <= n; i++) {
-            // Print leading spaces
-            for (int j = n; j > i; j--) {
-                System.out.print(" ");
+        for(int i=0;i<n;i++){
+            for(int j=0;j<i+1;j++){
+                System.out.print("* ");
             }
-            // Print stars
-            for (int k = 1; k <= (2 * i - 1); k++) {
-                System.out.print("*");
-            }
-            // Move to the next line after each row
             System.out.println();
         }
+        
     }
 }
