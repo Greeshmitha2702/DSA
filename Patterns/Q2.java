48cf3d3b01c84bb4fec072362fc5d8c8bd4e47b8
greeshmitha2702 <bingumallagreeshmitha@gmail.com>
2025-11-26 16:23:19 +0530

    2nd pattern

--- Patterns/Q2.java
+++ Patterns/Q2.java
@@ -1,5 +1,20 @@
+/*
+*****
+****
+***
+**  
+*
+ */
 package Patterns;
 
 public class Q2 {
-    
+    public static void main(String[] args) {
+        int n=5;
+        for (int i=0;i<n;i++){
+            for(int j=n;j>i;j--){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
 }
