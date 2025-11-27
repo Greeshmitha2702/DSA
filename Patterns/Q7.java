e6d6db0043f3ab155a707d5405cd382065d57940
greeshmitha2702 <bingumallagreeshmitha@gmail.com>
2025-11-27 19:02:49 +0530

    removed unused variables in pattern 7

--- Patterns/Q7.java
+++ Patterns/Q7.java
@@ -4,8 +4,6 @@ package Patterns;
 public class Q7 {
     public static void main(String[] args) {
         int n=5;
-        int spb=0;
-        int spa=n-1;
         for(int i=0;i<n;i++){
             for(int j=0;j<i;j++){
                 System.out.print("\t");
