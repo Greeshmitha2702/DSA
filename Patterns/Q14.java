80d80ecc91aabf309d7d6909b050b2903312dbe6
greeshmitha2702 <bingumallagreeshmitha@gmail.com>
2025-11-28 12:15:10 +0530

    to print table of a number

---
+++ Patterns/Q14.java
@@ -0,0 +1,10 @@
+package Patterns;
+
+public class Q14 {
+    public static void main(String[] args) {
+        int n=5;
+        for(int i=1;i<=10;i++){
+            System.out.println(n+" * "+i+" = "+(n*i));
+        }
+    }
+}
