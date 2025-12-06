33a21f7a2024ec85e9fb6c67c11cb96f8c154216
greeshmitha2702 <bingumallagreeshmitha@gmail.com>
2025-12-06 11:52:23 +0530

    duplicate parenthesis

---
+++ Stack/Q1.java
@@ -0,0 +1,32 @@
+package Stack;
+
+import java.util.*;
+
+public class Q1 {
+   public static void main(String[] args) {
+        Scanner s=new Scanner(System.in);
+        String string=s.nextLine();
+        s.close();
+        Stack<Character> stack=new Stack<>();
+        int i=0;
+        while(i<string.length()){ 
+            char c=string.charAt(i);
+            if(c==')'){
+                if(stack.peek()=='('){
+                    System.out.println(true);
+                    return;
+                }
+                else{
+                    while(stack.peek()!='('){
+                        stack.pop();
+                    }
+                    stack.pop();//to pop the opening bracket
+                }
+            }
+            else
+                stack.push(c);
+            i++;
+        }
+        System.out.println(false);
+   }
+}
