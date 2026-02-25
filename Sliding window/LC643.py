9af5523fee7576967a42e23f5d4c620febd8a250
Greeshmitha2702 <bingumallagreeshmitha@gmail.com>
2026-02-25 18:24:04 +0530

    Sliding window: maximum sum of sub array

--- Sliding window/LC643.py
+++ Sliding window/LC643.py
@@ -3,9 +3,12 @@ arr=[1,12,-5,-6,50,3]
 n = len(arr)
 sum = 0
 k = 4
+#First Window sum
 for i in range(k):
     sum += arr[i]
+#Initialize max sum
 maxSum = sum
+#Sliding the window
 for i in range(k,n):
     sum += arr[i]
     sum -= arr[i-k]
