60d1980d3930f3c40aa805a6810c7268574be687
Greeshmitha2702 <bingumallagreeshmitha@gmail.com>
2026-03-02 18:36:18 +0530

    sub array product less than k

--- Sliding window/Beauty.py
+++ Sliding window/Beauty.py
@@ -3,22 +3,4 @@
 # x-th smallest integer in the subarray where x is equal to the given integer.
 #If there are fewer than x elements in the subarray, the beauty is defined as 0.
 #Return an integer array containing the beauty of each subarray of size k in the order they appear in nums.
-nums = [1, -1, -3, -2, 3]
-k = 3
-x = 2
-n = len(nums)
-count = [0] * 101
-res = []
-for i in range(n):
-    if i < k:
-        count[nums[i] + 50] += 1
-    if i >= k - 1:
-        total = 0
-        beauty = 0
-        for num in range(-50,0):
-            total += count[num + 50]
-            if total >= x:
-                beauty = num
-                break
-        res.append(beauty)
-print(res)
+
